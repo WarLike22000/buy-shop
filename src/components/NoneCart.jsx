193ff2840891f61910baa81eb8dcb7d1f4e3254b
Alireza Kamali <kamalialireza788@gmail.com>
2023-04-27 19:41:27 +0430

    cgange all color input

--- src/components/NoneCart.jsx
+++ src/components/NoneCart.jsx
@@ -6,7 +6,7 @@ import { Button } from '@mui/material';
 
 const NoneCart = () => {
     return (
-        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column'}}>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', flexDirection: 'column', marginTop: '60px'}}>
             <img style={{width: '300px'}} src={empity} alt='empityShop' />
             <Link style={{textDecoration: 'none'}} to='/'><Button>بازگشت به صفحه اصلی</Button></Link>
         </div>
